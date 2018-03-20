0e8257dac9c10b400552afec5a20aba7ceecb601
cleopoochi <37578104+cleopoochi@users.noreply.github.com>
2018-03-20 16:27:53 +0530

    Update Vowel.py

--- Vowel.py
+++ Vowel.py
@@ -1,5 +1,5 @@
 a=char(input())
-if(a='a'||a='e'||a='i'||a='o'||a='u'):
+if(a=='a' or a=='e' or a=='i' or a=='o' or a=='u'or a=='A' or a=='E' or a=='I' or a=='O' or a=='U'):
   print("Vowel")
 else:
   print("not a Vowel")
